0569019ba853b34fe7746330083f0c9a38562653
Raghu-Rajalakshmi <raghurajalakshmi563@gmail.com>
2026-06-21 22:53:41 +0530

    Add files via upload

---
+++ to organize the file using two level directory structure. exp 15.cpp
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char mainDirectory[] = "C:/Users/itssk/OneDrive/Desktop";
+    char subDirectory[] = "os";
+    char fileName[] = "example.txt";
+
+    char filePath[200];
+    char mainDirPath[200];
+
+    /* Create the path */
+    snprintf(mainDirPath,
+             sizeof(mainDirPath),
+             "%s/%s/",
+             mainDirectory,
+             subDirectory);
+
+    snprintf(filePath,
+             sizeof(filePath),
+             "%s%s",
+             mainDirPath,
+             fileName);
+
+    /* Create the file */
+    FILE *file = fopen(filePath, "w");
+
+    if (file == NULL)
+    {
+        printf("Error creating file.\n");
+        return 1;
+    }
+
+    fprintf(file, "This is an example file content.");
+
+    fclose(file);
+
+    printf("File created successfully: %s\n", filePath);
+
+    return 0;
+}
